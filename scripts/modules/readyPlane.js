10df657452fb7c20b687a73f49f1a2896cbeecea
Konstantin <laedo303@mail.ru>
2022-02-03 18:29:36 +0500

    day_3

--- scripts/modules/readyPlane.js
+++ scripts/modules/readyPlane.js
@@ -2,7 +2,7 @@ import getFormPerson from "./formPerson.js";
 import airplane from "./airplane.js";
 
 
-const readyPlane = (forms, main) => {
+const readyPlane = (forms, main, tourData) => {
   const data = [];
 
   forms.forEach((form) => {
@@ -20,7 +20,7 @@ const readyPlane = (forms, main) => {
 
       if (forms.length === data.length) {
         forms.forEach(form => form.remove());
-        airplane(main, data);
+        airplane(main, data, tourData);
       };
     })
   });
